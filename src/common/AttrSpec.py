55ec3c03a49bc91d7945550bd1094a134f88620a
Matěj Barnat <barnama1@fit.cvut.cz>
2020-03-20 12:46:12 +0100

    Code refactoring
    Added support and validation for 'array' and 'set' data types

--- src/common/AttrSpec.py
+++ src/common/AttrSpec.py
@@ -1,6 +1,22 @@
 import yaml
-import time
 import ipaddress
+import re
+
+# Path to yaml file containing attribute specification
+path_attr_spec = "C:/Users/bfu1/Desktop/ADiCT/AttrSpec.yml"
+
+# List of primitive data types
+supported_data_types = [
+    "tag",
+    "binary",
+    "category",
+    "string",
+    "int",
+    "float",
+    "ipv4",
+    "ipv6",
+    "special"
+]
 
 # Default specification fields
 default_data_type = None
@@ -11,48 +27,37 @@ default_color = "#000000"
 default_description = ""
 default_confidence = False
 default_multi_value = False
-default_timestamp = True
+default_timestamp = False
 default_history = False
-default_max_age = "30d"
-default_max_items = 1000
 
-# Default record fields
-default_type = None
-default_id = None
-default_attr = None
-default_v = None
-default_t1 = None
-default_c = 1
-default_src = "Unknown"
-
-# List of supported data types
-supported_data_types = {
-    "tag",
-    "binary",
-    "category",
-    "string",
-    "int",
-    "float",
-    "ipv4",
-    "ipv6",
-    "special"
-    # TODO:
-    # Array, set and link types
-    # Maybe use multi_value to indicate that value field is an array/list? If so, what about set?
-    # How to represent link?
-}
+# Default history params
+default_max_age = None
+default_max_items = None
+default_expire_time = "inf"
 
 
-# Dictionary containing attribute specifications ('attr_id' -> AttrSpec)
-# Must be initialized by load_yaml()
-attr_spec = {}
+# Check whether given data type represents an array
+def is_array(data_type):
+    for t in supported_data_types:
+        if re.match(rf"^array<{t}>$", data_type):
+            return True
+    return False
 
 
-# Initialize 'attr_spec' dictionary from yaml config file
-def load_yaml(yaml_path):
-    d = yaml.safe_load(open(yaml_path))
-    for k in d:
-        attr_spec[k] = AttrSpec(k, d[k])
+# Check whether given data type represents a set
+def is_set(data_type):
+    for t in supported_data_types:
+        if re.match(rf"^set<{t}>$", data_type):
+            return True
+    return False
+
+
+# Check whether given data type represents a link
+def is_link(data_type):
+    for t in supported_data_types:
+        if re.match(rf"^link<{t}>$", data_type):
+            return True
+    return False
 
 
 # Validate ipv4 string
@@ -60,7 +65,7 @@ def valid_ipv4(address):
     try:
         ipaddress.IPv4Address(address)
         return True
-    except:
+    except ValueError:
         return False
 
 
@@ -69,107 +74,50 @@ def valid_ipv6(address):
     try:
         ipaddress.IPv6Address(address)
         return True
-    except:
+    except ValueError:
         return False
 
 
-# Validate record fields according to attribute specification
-def validate_record(record):
-    # Check mandatory fields
-    if type(record["type"]) is not str or \
-       type(record["id"]) is not str or \
-       type(record["attr"]) is not str:
-        raise TypeError
-
-    # Check whether 'attr_spec' contains the attribute
-    if record["attr"] not in attr_spec:
-        raise ValueError
-
-    spec = attr_spec[record["attr"]]
-
-    if spec.timestamp is True:
-        try:
-            # Try parsing timestamp values
-            t1 = time.strptime(record["t1"], spec.timestamp_format)
-            t2 = time.strptime(record["t2"], spec.timestamp_format)
-
-            # Check valid time interval (T2 must be greater than or equal to T1)
-            if t2 < t1:
-                raise ValueError
-        except:
-            raise
-
-    if spec.confidence is True:
-        if type(record["c"]) is not float:
-            raise TypeError
-        if record["c"] < 0 or record["c"] > 1:
-            raise ValueError
-
-    # Dictionary containing validator functions for supported data types
-    validators = {
-        "tag": lambda v: True,
-        "binary": lambda v: v in {True, False, None},
-        "category": lambda v: v in spec.categories,
-        "string": lambda v: type(v) is str,
-        "int": lambda v: type(v) is int,
-        "float": lambda v: type(v) is float,
-        "ipv4": valid_ipv4,
-        "ipv6": valid_ipv6,
-        "special": lambda v: v is not None
-    }
-
-    # Check data type of value field
-    f = validators.get(spec.data_type, lambda v: False)
-    if not f(record["v"]):
-        raise TypeError
+# Validate array object
+def valid_array(obj, validator):
+    if type(obj) is not list:
+        return False
+    for item in obj:
+        if not validator(item):
+            return False
+    return True
 
 
-# Create a record (from JSON/dict) according to attribute specification
-def make_record(r):
-    # Set missing fields to default values
-    record = {
-        "type": r.get("type", default_type),
-        "id": r.get("id", default_id),
-        "attr": r.get("attr", default_attr),
-        "t1": r.get("t1", default_t1),
-        "t2": r.get("t2", r.get("t1", default_t1)),
-        "v": r.get("v", default_v),
-        "c": r.get("c", default_c),
-        "src": r.get("src", default_src)
-    }
+# Validate set object
+def valid_set(obj, validator):
+    if type(obj) is not list:
+        return False
+    for item in obj:
+        if not validator(item) or obj.count(item) > 1:
+            return False
+    return True
 
-    try:
-        # Validate the record
-        validate_record(record)
-    except:
-        raise
-    return record
+
+# TODO Validate link object
+def valid_link(obj):
+    return False
+
+
+# Load attribute specification (from yaml) and return it as a dict ('attr_id' -> AttrSpec)
+# Raise TypeError or ValueError if the specification of some attribute is invalid
+def load_spec():
+    spec = yaml.safe_load(open(path_attr_spec))
+    attr_spec = {}
+    for attr in spec:
+        attr_spec[attr] = AttrSpec(attr, spec[attr])
+
+    return attr_spec
 
 
 # This class represents specification of an attribute of given id
 class AttrSpec:
-    # Validate attribute specification
-    def valid_spec(self):
-        # Data type must be supported
-        if self.data_type not in supported_data_types:
-            return False
-
-        # If value is of category type, spec must contain a list of valid categories
-        if self.data_type == "category" and type(self.categories) is not list:
-            return False
-
-        # If history is enabled, spec must contain a dict of history parameters
-        if self.history is True:
-            if type(self.history_params) is not dict:
-                return False
-            if "max_age" not in self.history_params:
-                self.history_params["max_age"] = default_max_age
-            if "max_items" not in self.history_params:
-                self.history_params["max_items"] = default_max_items
-            # TODO other history params
-        return True
-
     # Initialize member variables and validate attribute specification
+    # Raise TypeError or ValueError if the specification is invalid (e.g. data type is not supported)
     def __init__(self, id, spec):
         self.id = id
         self.name = spec.get("name", id)
@@ -184,5 +132,62 @@ class AttrSpec:
         self.history_params = spec.get("history_params", default_history_params)
         self.timestamp_format = spec.get("timestamp_format", default_timestamp_format)
 
-        if not self.valid_spec():
-            raise ValueError
+        # Dictionary containing validator functions for primitive data types
+        validators = {
+            "tag": lambda v: True,
+            "binary": lambda v: v in {True, False, None},
+            "category": lambda v: v in self.categories,
+            "string": lambda v: type(v) is str,
+            "int": lambda v: type(v) is int,
+            "float": lambda v: type(v) is float,
+            "ipv4": valid_ipv4,
+            "ipv6": valid_ipv6,
+            "special": lambda v: v is not None
+        }
+
+        # Initialize attribute's validator function according to its data type
+        if self.data_type in supported_data_types:
+            self.validator = validators[self.data_type]
+
+        elif is_array(self.data_type):
+            dtype = self.data_type.split("<")[1].split(">")[0]
+            self.validator = lambda v: valid_array(v, validators[dtype])
+
+        elif is_set(self.data_type):
+            dtype = self.data_type.split("<")[1].split(">")[0]
+            self.validator = lambda v: valid_set(v, validators[dtype])
+
+        elif is_link(self.data_type):
+            self.validator = lambda v: valid_link(v)
+
+        else:
+            raise ValueError("Type '{}' is not supported".format(self.data_type))
+
+        # If value is of category type, spec must contain a list of valid categories
+        if self.data_type == "category" and type(self.categories) is not list:
+            raise TypeError("Data type of 'categories' is invalid (should be list)")
+
+        # If history is enabled, spec must contain a dict of history parameters
+        if self.history is True:
+            if type(self.history_params) is not dict:
+                raise TypeError("Data type of 'history_params' is invalid (should be dict)")
+
+            if "max_age" in self.history_params:
+                if not re.match(r"(^0$|^[0-9]+[smhd]$)", str(self.history_params["max_age"])):
+                    raise ValueError("Value of 'max_age' is invalid")
+            else:
+                self.history_params["max_age"] = default_max_age
+
+            if "max_items" in self.history_params:
+                if type(self.history_params["max_items"]) is not int or self.history_params["max_items"] <= 0:
+                    raise ValueError("Value of 'max_items' is invalid")
+            else:
+                self.history_params["max_items"] = default_max_items
+
+            if "expire_time" in self.history_params:
+                if not re.match(r"(^0$|^inf$|^[0-9]+[smhd]$)", str(self.history_params["expire_time"])):
+                    raise ValueError("Value of 'expire_time' is invalid")
+            else:
+                self.history_params["expire_time"] = default_expire_time
+
+            # TODO other history params

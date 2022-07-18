eaebc6f40f715a20d25f31490074e7026e801fe4
OndrejSedlacek <xsedla1o@stud.fit.vutbr.cz>
2022-07-18 12:28:30 +0200

    Test entity type: improved API tests.

--- api/tests/test_probability.py
+++ api/tests/test_probability.py
@@ -24,39 +24,47 @@ class ProbabilityAttrSingle(unittest.TestCase):
             except Exception as e:
                 return f"error: {e}"
 
-        print(f"Testing v={v}")
-        response = request(f"test_entity_type/test_entity_id/test_attr_probability", f"v={json.dumps(v)}")
-        print(f"Response: {response.content}")
+        response = request(f"test_entity_type/test_entity_id/test_attr_probability", f"v={v}")
         return response
 
     def test_valid_format(self):
         v = {"A": 0.6, "B": 0.3, "C": 0.05, "D": 0.05}
-        response = self.helper_send_to_single(v)
+        response = self.helper_send_to_single(json.dumps(v))
         self.assertEqual(200, response.status_code)
 
     def test_invalid_format(self):
         for v in invalid_values:
-            response = self.helper_send_to_single(v)
-            self.assertEqual(400, response.status_code)
+            with self.subTest(v=v):
+                response = self.helper_send_to_single(json.dumps(v))
+                self.assertEqual(400, response.status_code)
+
+    def test_invalid_json(self):
+        v = "{'A':1.0}"
+        response = self.helper_send_to_single(v)
+        self.assertEqual(400, response.status_code)
 
 
 class ProbabilityAttrMultiple(unittest.TestCase):
     @staticmethod
     def helper_send_to_multiple(v):
-        print(f"Testing v={v}")
         response = requests.post(f"{base_url}/datapoints", json=[{
             "type": "test_entity_type", "id": "test_entity_id", "attr": "test_attr_probability",
-            "t1": datetime.datetime.now().strftime("%Y-%m-%dT%H-%M-%S"), "v": json.dumps(v)
+            "t1": datetime.datetime.now().strftime("%Y-%m-%dT%H-%M-%S"), "v": v
         }])
-        print(f"Response: {response.content}")
         return response
 
     def test_valid_format_multiple(self):
         v = {"A": 0.6, "B": 0.3, "C": 0.05, "D": 0.05}
-        response = self.helper_send_to_multiple(v)
+        response = self.helper_send_to_multiple(json.dumps(v))
         self.assertEqual(200, response.status_code)
 
     def test_invalid_format_multiple(self):
         for v in invalid_values:
-            response = self.helper_send_to_multiple(v)
-            self.assertEqual(400, response.status_code)
+            with self.subTest(v=v):
+                response = self.helper_send_to_multiple(json.dumps(v))
+                self.assertEqual(400, response.status_code)
+
+    def test_invalid_json(self):
+        v = "{'A':1.0}"
+        response = self.helper_send_to_multiple(v)
+        self.assertEqual(400, response.status_code)

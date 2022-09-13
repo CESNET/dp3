1e3d923934ba1cbc40884d1a942e68606e80d94e
OndrejSedlacek <xsedla1o@stud.fit.vutbr.cz>
2022-09-13 12:19:36 +0200

    Minor adjustment to test values.

--- api/tests/common.py
+++ api/tests/common.py
@@ -25,10 +25,10 @@ values = {
         "float": ["1.0", "1"],
         "ipv4": ["\"127.0.0.1\""],
         "ipv6": ["\"2001:0db8:85a3:0000:0000:8a2e:0370:7334\"", "\"::1\""],
-        "mac": ["\"de:ad:be:ef:ba:be\"", "\"11:22:33:44:55:66\""],
+        "mac": ["de:ad:be:ef:ba:be", "11:22:33:44:55:66"],
         "time": ["\"2020-01-01T00:00:00\""],
         "json": ["{\"test\": \"test\"}"],
-        "category": ["\"cat1\""],
+        "category": ["cat1"],
         "array": ["[1,2,3]"],
         "set": ["[1,2,3]"],
         "dict": ["{\"key1\":1,\"key2\":\"xyz\"}"]
@@ -37,7 +37,7 @@ values = {
         "binary": ["xyz"],
         "int": ["xyz"],
         "int64": ["xyz"],
-        "string": ["xyz"],  # missing double quotes
+        "string": [],  # all JSON strings can be converted to strings
         "float": ["xyz"],
         "ipv4": ["\"xyz\""],
         "ipv6": ["\"xyz\""],
--- api/tests/test_push_single.py
+++ api/tests/test_push_single.py
@@ -1,4 +1,5 @@
 import requests
+
 import common
 
 base_url = None

0378a25e79980b48d04a5e022cfe3e3bc89800b2
Dávid Benko <david.benko@vut.cz>
2023-01-17 11:16:42 +0100

    Remove history management and unnecessary database schemas

--- dp3/database/database.py
+++ dp3/database/database.py
@@ -8,42 +8,10 @@ from dp3.common.attrspec import AttrSpec
 from dp3.common.config import HierarchicalDict
 from dp3.common.entityspec import EntitySpec
 
-# Static preconfiguration of attribute's history table
-HISTORY_ATTRIBS_CONF = {
-    "id": AttrSpec("id", {"name": "id", "type": "plain", "data_type": "int"}),
-    "eid": AttrSpec("eid", {"name": "eid", "type": "plain", "data_type": "string"}),
-    # "value" is inserted manually, because it depends on the attribute
-    "t1": AttrSpec("t1", {"name": "t1", "type": "plain", "data_type": "time"}),
-    "t2": AttrSpec("t2", {"name": "t2", "type": "plain", "data_type": "time"}),
-    "c": AttrSpec("c", {"name": "c", "type": "plain", "data_type": "float"}),
-    "src": AttrSpec("src", {"name": "src", "type": "plain", "data_type": "string"}),
-
-    "tag": AttrSpec("tag", {"name": "tag", "type": "plain", "data_type": "int"})
-}
-
-# Preconfigured attributes all tables (records) should have
-TABLE_MANDATORY_ATTRIBS = {
-    "ts_added": AttrSpec("ts_added", {"name": "timestamp of record creation", "type": "plain", "data_type": "time"}),
-    "ts_last_update": AttrSpec("ts_last_update", {"name": "timestamp of record last update", "type": "plain", "data_type": "time"}),
-}
-
-# Preconfiguration of main entity tables
-EID_CONF = {"eid": AttrSpec("eid", {"name": "entity id", "type": "plain", "data_type": "string"})}
-
-# number of seconds to wait for the i-th attempt to reconnect after error
-RECONNECT_DELAYS = [1, 2, 5, 10, 30]
-
-# Custom exceptions
-# class DatabaseConfigMismatchError(Exception):
-#     def __init__(self, message=None):
-#         if message is None:
-#             message = "Database configuration is not valid, current configuration is not the same with current database " \
-#                       "server schema configuration. Migration is not supported yet, drop all the updated tables" \
-#                       " first, if you want to apply your configuration!"
-#         super().__init__(message)
 
 class DatabaseError(Exception):
     pass
+
 
 class MissingTableError(DatabaseError):
     pass

f06dc68aae3b268ef5a58f55b148d54e3592d125
Dávid Benko <david.benko@vut.cz>
2023-01-17 14:08:49 +0100

    Enhance timeseries `DataPoint` validation and fix typos

--- dp3/common/datapoint.py
+++ dp3/common/datapoint.py
@@ -3,7 +3,7 @@ from typing import Any, Optional, Union
 
 from pydantic import BaseModel, confloat
 
-from dp3.common.attrspec import AttrSpec, AttrType
+from dp3.common.attrspec import AttrSpecGeneric, AttrType
 from dp3.common.entityspec import EntitySpec
 
 
@@ -31,7 +31,7 @@ class DataPoint(BaseModel):
 
     def __init__(self, attr_type: AttrType, **data):
         # Plain attributes don't have any t1 and t2.
-        # Give this fields any value, so it passes validation.
+        # Let be t1 and t2 current timestamps, as the change was made now.
         if attr_type == AttrType.PLAIN:
             default_values = {
                 "t1": datetime.now(),
@@ -45,12 +45,13 @@ class DataPoint(BaseModel):
 
         super().__init__(attr_type=attr_type, **data)
 
-    def validate_against_attr_spec(self, etype: str, attr_spec: dict[str, dict[str, Union[EntitySpec, dict[str, AttrSpec]]]]):
+    def validate_against_attr_spec(self, etype: str, attr_spec: dict[str, dict[str, Union[EntitySpec, dict[str, AttrSpecGeneric]]]]):
         """Validates self against provided attributes specification
 
         This method is called when creating Task.
         """
-        assert self.etype == etype, f"etype of task and contained datapoint don't match: {self.etype} != {etype}"
+        assert self.etype == etype, \
+            f"etype of task and contained datapoint don't match: {self.etype} != {etype}"
         assert self.etype in attr_spec, f"Invalid etype '{self.etype}'"
         assert self.attr in attr_spec[self.etype]["attribs"], \
             f"Invalid attribute '{self.attr}' of entity '{self.etype}'"
@@ -59,3 +60,18 @@ class DataPoint(BaseModel):
 
         assert self.attr_type == attrib_spec.t, \
             f"Invalid attribute type: DP has '{self.attr_type}', but attribute '{self.attr}' of entity '{self.etype}' has {attrib_spec.t}"
+
+        if self.attr_type == AttrType.OBSERVATIONS:
+            # Check if all value arrays are the same length
+            values_len = [ len(v_i) for _, v_i in self.v.items() ]
+            assert len(set(values_len)) == 1, f"Datapoint arrays have different lengths: {values_len}"
+
+            # Check t2
+            if attrib_spec.timeseries_type == "regular":
+                time_step = attrib_spec.timeseries_params.time_step
+                assert t2 - t1 == values_len[0] * time_step, \
+                    f"Difference of t1 and t2 is invalid. Must be n*time_step."
+
+            # Check all series are present
+            for series_id in attrib_conf.series:
+                assert series_id in v, f"Datapoint is missing values for '{series_id}' series"

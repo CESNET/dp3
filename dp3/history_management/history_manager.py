7695efd1e2bbdba45bcf0ba75dda3bea0737b528
OndrejSedlacek <xsedla1o@stud.fit.vutbr.cz>
2023-02-01 13:32:13 +0100

    history_manager.py: use simpler iteration API.

--- dp3/history_management/history_manager.py
+++ dp3/history_management/history_manager.py
@@ -38,21 +38,21 @@ class HistoryManager:
         """Deletes old data points from master collection."""
         self.log.debug("Deleting old records ...")
 
-        for etype in self.model_spec:
-            for attr_name, attr_conf in self.model_spec.attribs(etype).items():
-                max_age = None
+        for etype_attr, attr_conf in self.model_spec.attributes.items():
+            etype, attr_name = etype_attr
+            max_age = None
 
-                if attr_conf.t == AttrType.OBSERVATIONS:
-                    max_age = attr_conf.history_params.max_age
-                elif attr_conf.t == AttrType.TIMESERIES:
-                    max_age = attr_conf.timeseries_params.max_age
+            if attr_conf.t == AttrType.OBSERVATIONS:
+                max_age = attr_conf.history_params.max_age
+            elif attr_conf.t == AttrType.TIMESERIES:
+                max_age = attr_conf.timeseries_params.max_age
 
-                if not max_age:
-                    continue
+            if not max_age:
+                continue
 
-                t_old = datetime.utcnow() - max_age
+            t_old = datetime.utcnow() - max_age
 
-                try:
-                    self.db.delete_old_dps(etype, attr_name, t_old)
-                except DatabaseError as e:
-                    self.log.error(e)
+            try:
+                self.db.delete_old_dps(etype, attr_name, t_old)
+            except DatabaseError as e:
+                self.log.error(e)

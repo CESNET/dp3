49e44c38dfb38eb0f6fae52257d92ef9a22ea0cf
Dávid Benko <david.benko@vut.cz>
2023-02-14 23:12:21 +0100

    Fix WSGI
    Apache's mod_wsgi sets environmental variables differently.
    This fix has been probably implemented for a long time, but was
    never pushed to git.

--- api/wsgi.py
+++ api/wsgi.py
@@ -1,3 +1,14 @@
-from receiver import app as application
+import os
 
-application.debug = True
+
+def application(environ, start_response):
+    # Set environmental variables
+    # mod_wsgi passes "environmental" variables from SetEnv using `environ` variable
+    for key in ["DP3_APP_NAME", "DP3_CONFIG_DIR", "DP3_DP_LOG_FILE"]:
+        os.environ[key] = environ.get(key, "")
+
+    from receiver import app as _application
+
+    _application.debug = True
+
+    return _application(environ, start_response)

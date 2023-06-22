f6622764035123a1d18bdb60ef320fa5c467e3c4
OndrejSedlacek <xsedla1o@stud.fit.vutbr.cz>
2023-06-22 15:36:33 +0200

    Automation: Added DP3 application template and setup script.
    Updated example config.

---
+++ dp3/bin/setup.py
@@ -0,0 +1,50 @@
+"""DP3 Setup Script for creating a DP3 application."""
+import argparse
+import shutil
+from pathlib import Path
+
+
+def replace_template(directory: Path, template: str, replace_with: str):
+    """Replace all occurrences of `template` with the given text."""
+    for file in directory.rglob("*"):
+        if file.is_file():
+            try:
+                with file.open("r+") as f:
+                    contents = f.read()
+                    contents = contents.replace(template, replace_with)
+                    f.seek(0)
+                    f.write(contents)
+                    f.truncate()
+            except UnicodeDecodeError:
+                print(f"Skipping {file} as it is a binary file.")
+
+
+def main():
+    # Create an argparse object to parse the project directory and the app name.
+    parser = argparse.ArgumentParser(description="Create a DP3 application.")
+    parser.add_argument("project_dir", help="The project directory.", type=str)
+    parser.add_argument("app_name", help="The name of the application.", type=str)
+    args = parser.parse_args()
+    app_name = args.app_name.lower()
+
+    # Ensure the project directory exists.
+    project_dir = Path(args.project_dir)
+    project_dir = project_dir.absolute()
+    project_dir.mkdir(exist_ok=True, parents=True)
+
+    # Get the current package location.
+    package_dir = Path(__file__).parent.parent.parent
+
+    directories_to_copy = ["config"]
+    for directory in directories_to_copy:
+        shutil.copytree(package_dir / directory, project_dir / directory, dirs_exist_ok=True)
+
+    # Copy the template files to the project directory.
+    shutil.copytree(package_dir / "install" / "template", project_dir, dirs_exist_ok=True)
+
+    replace_template(project_dir, "{{DP3_APP}}", app_name)
+    replace_template(project_dir, "__dp3_app__", app_name)
+
+
+if __name__ == "__main__":
+    main()
---
+++ install/template/docker/rabbitmq/init-rmq.sh
@@ -0,0 +1,78 @@
+#!/bin/bash
+# Usage: export DP3_APP_NAME=<appname>; export DP3_WORKER_NUM=<N>; ./init-rmq.sh
+
+# Check that required environment variables are set
+if [ -z "$DP3_APP_NAME" ]; then
+  echo "Missing environment variable: DP3_APP_NAME (expected value is application name)";
+else
+  APPNAME=$DP3_APP_NAME
+fi
+if [ -z "$DP3_WORKER_NUM" ]; then
+  echo "Missing environment variable: DP3_WORKER_NUM (expected value is number of workers)";
+else
+  N=$DP3_WORKER_NUM
+fi
+if [ -z "$DP3_APP_NAME" ] || [ -z "$DP3_WORKER_NUM" ]; then
+  exit 1
+fi
+
+# set -m enables job control, which we need to bring the rabbitmq-server process to foreground after configuration
+set -m
+
+# start server in background and wait until initialized
+rabbitmq-server &
+rabbitmqctl wait --timeout 60 "$RABBITMQ_PID_FILE"
+
+echo "** Removing all DP3 exchanges and queues for app '${APPNAME}' **"
+
+exchange_list=$(rabbitmqadmin list exchanges name -f tsv | grep "^${APPNAME}-.*-task-exchange")
+for q in $exchange_list
+do
+  rabbitmqadmin delete exchange "name=$q"
+done
+
+queue_list=$(rabbitmqadmin list queues name -f tsv | grep "^${APPNAME}-worker-")
+for q in $queue_list
+do
+  rabbitmqadmin delete queue "name=$q"
+done
+
+if [[ "$N" -eq 0 ]]; then
+  exit 0
+fi
+
+echo "** Setting up exchanges and queues for $N workers **"
+
+# Declare exchanges for datapoint tasks (normal and priority) and for snapshots
+rabbitmqadmin declare exchange "name=${APPNAME}-main-task-exchange" type=direct durable=true
+rabbitmqadmin declare exchange "name=${APPNAME}-priority-task-exchange" type=direct durable=true
+
+rabbitmqadmin declare exchange "name=${APPNAME}-main-snapshot-exchange" type=direct durable=true
+
+# Declare exchange for control tasks
+rabbitmqadmin declare exchange "name=${APPNAME}-control-exchange" type=direct durable=true
+
+# Declare queues for N workers
+for i in $(seq 0 $((N-1)))
+do
+  rabbitmqadmin declare queue "name=${APPNAME}-worker-$i" durable=true 'arguments={"x-max-length": 10000, "x-overflow": "reject-publish"}'
+  rabbitmqadmin declare queue "name=${APPNAME}-worker-$i-pri" durable=true
+
+  rabbitmqadmin declare queue "name=${APPNAME}-worker-$i-snapshots" durable=true
+done
+
+# Declare control queue
+rabbitmqadmin declare queue "name=${APPNAME}-control" durable=true
+
+# Bind queues to exchanges
+for i in $(seq 0 $((N-1)))
+do
+  rabbitmqadmin declare binding "source=${APPNAME}-main-task-exchange" "destination=${APPNAME}-worker-$i" routing_key=$i
+  rabbitmqadmin declare binding "source=${APPNAME}-priority-task-exchange" "destination=${APPNAME}-worker-$i-pri" routing_key=$i
+
+  rabbitmqadmin declare binding "source=${APPNAME}-main-snapshot-exchange" "destination=${APPNAME}-worker-$i-snapshots" routing_key=$i
+done
+rabbitmqadmin declare binding "source=${APPNAME}-control-exchange" "destination=${APPNAME}-control" routing_key=0
+
+# bring rabbitmq-server process to foreground
+fg

3d69c66d6f755c674a5ec858d010bfb05059c7eb
OndrejSedlacek <xsedla1o@stud.fit.vutbr.cz>
2023-06-23 12:57:22 +0200

    Installing: Removed reload from `api`.

--- dp3/bin/api.py
+++ dp3/bin/api.py
@@ -8,7 +8,7 @@ from dp3.api.main import app
 
 
 def run():
-    uvicorn.run(app, host="0.0.0.0", port=5000, reload=True)
+    uvicorn.run(app, host="0.0.0.0", port=5000)
 
 
 if __name__ == "__main__":

7d174ec46709b818ad4e55c55318327af7fcd913
OndrejSedlacek <xsedla1o@stud.fit.vutbr.cz>
2023-06-28 10:23:26 +0200

    API: Improved script entrypoint.

--- dp3/bin/api.py
+++ dp3/bin/api.py
@@ -2,13 +2,31 @@
 """
 Run the DP3 API using uvicorn.
 """
-import uvicorn
+import argparse
 
-from dp3.api.main import app
+import uvicorn
 
 
 def run():
-    uvicorn.run(app, host="0.0.0.0", port=5000)
+    parser = argparse.ArgumentParser(description="Run the DP3 API using uvicorn.")
+    parser.add_argument(
+        "--host",
+        type=str,
+        default="0.0.0.0",
+        help="The host to bind to. (default: 0.0.0.0)",
+    )
+    parser.add_argument(
+        "--port", type=int, default=5000, help="The port to bind to. (default: 5000)"
+    )
+    parser.add_argument(
+        "--reload",
+        action="store_true",
+        default=False,
+        help="Enable auto-reload of the api. (API changes will be picked up automatically.)",
+    )
+    args = parser.parse_args()
+
+    uvicorn.run("dp3.api.main:app", host=args.host, port=args.port, reload=args.reload)
 
 
 if __name__ == "__main__":

a88e2ba625ba40c2b2bcd4265d4e8c7d11019f02
Dávid Benko <david.benko@vut.cz>
2023-11-15 10:20:14 +0100

    API: use `isinstance` instead of `type` (fix Ruff error)

--- dp3/api/routers/entity.py
+++ dp3/api/routers/entity.py
@@ -64,7 +64,7 @@ async def list_entity_type_eids(
     if not fulltext_filters:
         fulltext_filters = {}
 
-    if type(fulltext_filters) is not dict:
+    if isinstance(fulltext_filters, dict):
         raise HTTPException(status_code=400, detail="Fulltext filter is invalid")
 
     for attr in fulltext_filters:

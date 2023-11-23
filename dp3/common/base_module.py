30a0c49f3949d5e1cc30df90b353de6f70081a59
OndrejSedlacek <xsedla1o@stud.fit.vutbr.cz>
2023-11-23 16:49:06 +0100

    BaseModule: Initialize logging and load config.

--- dp3/common/base_module.py
+++ dp3/common/base_module.py
@@ -1,3 +1,5 @@
+import logging
+
 from dp3.common.callback_registrar import CallbackRegistrar
 from dp3.common.config import PlatformConfig
 from dp3.common.state import SharedFlag
@@ -5,21 +7,36 @@ from dp3.common.state import SharedFlag
 
 class BaseModule:
     """
-    Abstract class for platform modules.
+    Base class for platform modules.
     Every module must inherit this abstract class for automatic loading of module!
+
+    Attributes:
+        refresh: SharedFlag that is set to True when module should refresh its attributes values
+        log: Logger for the module
     """
 
     def __init__(
         self, platform_config: PlatformConfig, module_config: dict, registrar: CallbackRegistrar
     ):
         """Initialize the module and register callbacks.
+
+        `self.load_config()` is called in the base class.
         Args:
             platform_config: Platform configuration class
             module_config: Configuration of the module,
                 equivalent of `platform_config.config.get("modules.<module_name>")`
             registrar: A callback / hook registration interface
         """
-        self.refresh = SharedFlag(False, banner=f"Refresh {self.__class__.__name__}")
+        self.refresh: SharedFlag = SharedFlag(False, banner=f"Refresh {self.__class__.__name__}")
+        self.log: logging.Logger = logging.getLogger(self.__class__.__name__)
+
+        self.load_config(platform_config, module_config)
+
+    def load_config(self, config: PlatformConfig, module_config: dict) -> None:
+        """Load module configuration.
+
+        Is called on module initialization and on refresh request via `/control` API.
+        """
 
     def start(self) -> None:
         """
@@ -38,6 +55,3 @@ class BaseModule:
         separate thread if it is used. Do nothing unless overridden.
         """
         return None
-
-    def load_config(self, config: PlatformConfig, module_config: dict) -> None:
-        """Load module configuration"""

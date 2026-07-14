931f5e9c9e7c4837c4659f7d8473ec5ded7972f3
Lukas Simonik <xsimonl00@stud.fit.vut.cz>
2026-07-14 12:45:20 +0200

    API TESTS(get_entity_edids): fix query param when test eid sort, add test for multi attr sort

--- tests/test_api/test_get_entity_eids.py
+++ tests/test_api/test_get_entity_eids.py
@@ -62,11 +62,67 @@ class GetEntityEids(common.APITest):
         )
 
     def test_get_entity_eids_sort_by_eid_asc(self):
-        eids = self.get_entity_data("entity/A/get", EntityEidList, sort=["eid:1"], limit=0)
+        eids = self.get_entity_data("entity/A/get", EntityEidList, sort="eid:1", limit=0)
         self.assertEqual(100, len(eids.data))
         self.assertEqual(list(range(100)), [x["eid"] for x in eids.data])
 
     def test_get_entity_eids_sort_by_eid_desc(self):
-        eids = self.get_entity_data("entity/A/get", EntityEidList, sort=["eid:-1"], limit=0)
+        eids = self.get_entity_data("entity/A/get", EntityEidList, sort="eid:-1", limit=0)
         self.assertEqual(100, len(eids.data))
         self.assertEqual(list(range(99, -1, -1)), [x["eid"] for x in eids.data])
+
+    def test_get_entity_eids_pagination_with_sort(self):
+        received_eids = []
+        for i in range(0, 100, 10):
+            eids = self.get_entity_data(
+                "entity/A/get", EntityEidList, sort="eid:1", skip=i, limit=10
+            )
+            self.assertEqual(10, len(eids.data), f"Failed at {i}")
+            received_eids.extend(x["eid"] for x in eids.data)
+        self.assertEqual(list(range(100)), received_eids)
+
+        received_eids = []
+        for i in range(0, 100, 10):
+            eids = self.get_entity_data(
+                "entity/A/get", EntityEidList, sort="eid:-1", skip=i, limit=10
+            )
+            self.assertEqual(10, len(eids.data), f"Failed at {i}")
+            received_eids.extend(x["eid"] for x in eids.data)
+        self.assertEqual(list(range(99, -1, -1)), received_eids)
+
+    def _get_sorted_eids(self, sort_params: list[str], **kwargs) -> EntityEidList:
+        """Fetch entity EIDs with multiple sort query parameters.
+
+        The shared `get_request` helper joins kwargs with '&' and cannot emit
+        multiple values for the same key, which is required for multi-column
+        sorting. Build the query string explicitly here.
+        """
+        query_parts = [f"sort={param}" for param in sort_params]
+        for key, value in kwargs.items():
+            query_parts.append(f"{key}={value}")
+        response = self.get_request(f"entity/A/get?{'&'.join(query_parts)}")
+        self.assertEqual(response.status_code, 200)
+        return EntityEidList.model_validate_json(response.content)
+
+    def test_get_entity_eids_sort_by_multiple_attrs(self):
+        res = self.push_datapoints(
+            [
+                {"src": "setup@test", "attr": "data2", "type": "A", "id": i, "v": f"g{i % 5}"}
+                for i in range(0, 100)
+            ]
+        )
+        self.assertEqual(res.status_code, 200)
+        sleep(8)
+        self.get_request("control/make_snapshots")
+        sleep(6)
+
+        expected = sorted(range(100), key=lambda i: (f"g{i % 5}", i))
+        # omit :1 if sort is ascending
+        eids = self._get_sorted_eids(["data2", "eid"], limit=0)
+        self.assertEqual(100, len(eids.data))
+        self.assertEqual(expected, [x["eid"] for x in eids.data])
+
+        expected = sorted(range(100), key=lambda i: (f"g{i % 5}", -i))
+        eids = self._get_sorted_eids(["data2:1", "eid:-1"], limit=0)
+        self.assertEqual(100, len(eids.data))
+        self.assertEqual(expected, [x["eid"] for x in eids.data])
